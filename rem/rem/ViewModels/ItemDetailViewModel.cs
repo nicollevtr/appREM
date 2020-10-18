662093902538a06602de331baaf3f8c3e033dd6e
nicollevtr <nicolle.vasconcelos@ime.eb.br>
2020-10-17 21:38:37 -0300

    versao inicial

---
+++ rem/rem/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using rem.Models;
+
+namespace rem.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
